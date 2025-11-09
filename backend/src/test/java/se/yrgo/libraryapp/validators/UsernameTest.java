0f1849d275e1bff57e66df517bfe0bb40f9368b0
André Ljungsvik <140394392+andreljungsvik@users.noreply.github.com>
2025-11-09 19:45:07 +0100

    UsernameTest

--- backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
@@ -1,17 +1,56 @@
 package se.yrgo.libraryapp.validators;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class UsernameTest {
-    @Test
-    void correctUsername() {
-        assertTrue(Username.validate("bosse"));
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class UsernameTest {
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "bosse",
+            "@ndre",
+            "1234567678876",
+            "abcdefghijklmnopqrstuvwxyz",
+            "@.-_-.@"
+    })
+    void validate_validUsernames_shouldReturnTrue(String username) {
+        assertThat(Username.validate(username)).isTrue();
     }
 
     @Test
-    void incorrectUsername() {
-        assertFalse(Username.validate("name with space"));
+    void validate_tooShortUsername_shouldReturnFalse() {
+        assertThat(Username.validate("bos")).isFalse();
+    }
+
+    @Test
+    void validate_forbiddenCharacter_shouldReturnFalse() {
+        assertThat(Username.validate("b#sse")).isFalse();
+    }
+
+    @Test
+    void validate_onlySpecialCharacters_shouldReturnFalse() {
+        assertThat(Username.validate("!@#$%^&*")).isFalse();
+    }
+
+    @Test
+    void validate_whitespace_shouldReturnFalse() {
+        assertThat(Username.validate(" ")).isFalse();
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    void validate_emptyString_shouldReturnFalse(String string) {
+        assertThat(Username.validate(string)).isFalse();
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void validate_nullValue_shouldReturnFalse(String string) {
+        assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> Username.validate(string));
     }
 }
