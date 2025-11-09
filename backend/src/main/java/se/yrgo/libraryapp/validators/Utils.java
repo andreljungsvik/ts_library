53d32f0cb7bb00c679984bc5628c5dac3496ed69
André Ljungsvik <140394392+andreljungsvik@users.noreply.github.com>
2025-11-09 19:48:58 +0100

    Utils

--- backend/src/main/java/se/yrgo/libraryapp/validators/Utils.java
+++ backend/src/main/java/se/yrgo/libraryapp/validators/Utils.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 /**
  * Utility functions for validation classes.
- * 
+ *
  */
 class Utils {
     private Utils() {}
@@ -12,7 +12,7 @@ class Utils {
     /**
      * Remove any non-alphabetic letters from a string, but keep any whitespace.
      * Will return the string as all lowecase.
-     * 
+     *
      * @param str the string to filter
      * @return a string with all non-letters removed (except whitespace)
      */
@@ -23,15 +23,15 @@ class Utils {
     }
 
     /**
-     * Converts any "leet speak" letters into their alphabetic equivalent (i.e. 4 to a etc.) and 
+     * Converts any "leet speak" letters into their alphabetic equivalent (i.e. 4 to a etc.) and
      * then removes any letters that are not alphabetic (but not whitespace). Will return the string
      * as all lowecase.
-     * 
+     *
      * @param str the string to clean
      * @return a string without non alphabetic characters (except whitespace)
      */
     static String cleanAndUnLeet(String str) {
-        final var leetMap = Map.of('1', 'l', '3', 'e', '4', 'a', '5', 's', '6', 'b', '7', 't', '8', 'b', '0', 'o');
+        final var leetMap = Map.of('1', 'i', '3', 'e', '4', 'a', '5', 's', '6', 'g', '7', 't', '8', 'b', '0', 'o');
 
         final StringBuilder res = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
